0e688646e08472e26142d34f636850c40852a181
Nurbolat <nurba.dav@gmail.com>
2022-05-20 00:33:05 +0600

    merge commit

---
+++ my-vue-app/src/components/Question.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent>
+        <p>{{question.title}}</p>
+        <div v-for="answer in question.answers" :key="answer">
+            <input v-if="question.type" type="radio" name="" id="">
+            <input v-else type="checkbox" name="" id="">
+            <p>{{answer}}</p>
+        </div>
+        <input type="submit" value="check" @click="check()">
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        question: {
+            type: Object,
+            required: true,
+        },
+    },
+
+    methods: {
+        check() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
